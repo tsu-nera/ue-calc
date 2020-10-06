72fe8381ee78688b540301666c77406ce06f093c
tsu-nera <fox10225fox@gmail.com>
2020-10-06 09:35:42 +0900

    wip

--- calc.py
+++ calc.py
@@ -12,15 +12,22 @@ df_sum = df.sum()
 print(df_sum)
 
 tips = df_sum["チップ"]
+
+# 売上
 sales = df_sum[["料金", "ピーク", "ブースト"]].sum()
+# サービス手数料
 fee = abs(df_sum["Uber 手数料 Jct を含む"])
+# 現金
+cash = 30648
 
 print()
 print(tips, sales, fee)
 
 print()
 
+# assert fee == cash, "test"
+
 print("売掛金:{}, 雑収入:{}".format(tips, tips))
 print("支払手数料:{}, 普通預金:{}".format(fee, fee))
-print("売掛金:{}, 売上高:{}".format(0, 0))
+print("売掛金:{}, 現金:{}, 売上高:{}".format(0, cash, sales))
 print("普通預金:{}, 売掛金:{}".format(0, 0))
